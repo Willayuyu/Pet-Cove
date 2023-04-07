dd8e4fd6d7f8c2187770188a62b6a402b3c705c9
Willayuyu <1394361375@qq.com>
2023-04-07 14:57:15 +0200

    Revert "1"
    This reverts commit d227997aaa7dddc49376c91272521c2cab11b3e6.

---
+++ .history/Front-end/src/Components/BuyerPage/BuyerHomePage_20230324235210.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container my-5" >
+        <div>
+            <b-card no-body style="min-height: 1000px;">
+                <div>
+                    <b-tabs class="ml-2 my-4" content-class="mx-5 my-3" justified vertical lazy>
+                        <AccountProfile/>
+                        <OrderPage/>
+                        <InventoryPage/>
+                        <b-tab title-item-class="my-1" title="Coming soon" disabled><p>I'm a disabled tab!</p></b-tab>
+                    </b-tabs>
+                </div>
+            </b-card>
+        </div>
+    </div>
+</template>
+
+<script>
+import AccountProfile from './AccountProfile/AccountProfile.vue';
+import OrderPage from './Orders/OrderPage.vue';
+
+export default {
+    components: {
+        AccountProfile,
+        OrderPage,
+    }
+}
+
+</script>
+
+<style>
+
+.tab {
+    margin: 20px;
+}
+
+
---
+++ .history/Front-end/src/router_20230324234520.js
@@ -0,0 +1,66 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const Home = () => import('@/views/Home.vue');
+const Products = () => import('@/views/Products.vue');
+const Contact = () => import('@/views/Contact.vue');
+const Info = () => import('@/views/Info.vue');
+const Login = () => import('@/views/Login.vue');
+const Register = () => import('@/views/Register.vue');
+
+const SellerHomePage = () => import('@/views/Seller/SellerHomePage.vue')
+const buyerHomePage =() => import('@/view/Buyer/')
+const TestPage = () => import('@/views/Test/TestPage.vue')
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },{
+      path: '/products',
+      name: 'Products',
+      component: Products
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/info',
+      name: 'Info',
+      component: Info
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/sellerHomePage',
+      name: 'SellerHomePage',
+      component: SellerHomePage
+    },
+    {
+      path: '/testPage',
+      name: 'TestPage',
+      component: TestPage
+    },
+    {
+      path: '/buyerHomePage',
+      name: 'buyerHomePage',
+      component: BuyerHomePage
+    },
+    
+  ],
+  mode: 'history'
+})
